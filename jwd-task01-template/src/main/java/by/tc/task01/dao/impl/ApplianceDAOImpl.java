3235f78bcc9c4c07144e9aac44bd92ee09f13090
Paulbel <pavelsinelnikovbel@gmail.com>
2017-10-08 13:12:33 +0300

    created appliance dao, using regex

--- jwd-task01-template/src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
+++ jwd-task01-template/src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
@@ -4,17 +4,64 @@ import by.tc.task01.dao.ApplianceDAO;
 import by.tc.task01.entity.Appliance;
 import by.tc.task01.entity.criteria.Criteria;
 
-public class ApplianceDAOImpl implements ApplianceDAO{
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	@Override
-	public <E> Appliance find(Criteria<E> criteria) {
-		// you may add your own code here
-		return null;
-	}
-	
-	// you may add your own code here
+public class ApplianceDAOImpl implements ApplianceDAO {
+    private String FILE_PATH = "jwd-task01-template/src/main/resources/appliances_db.txt";
+    @Override
+    public <E> Appliance find(Criteria<E> criteria) {
+        String className = "Oven";
+        BufferedReader br = null;
+        FileReader fr = null;
+        Pattern pattern = Pattern.compile(className + "\\s*");
+        Pattern pattern1 = Pattern.compile("(\\,|\\:)\\s(\\w+)\\=(((\\d+\\.*\\d*)+\\-*(\\d+\\.*\\d*)*)|((\\w+)(\\-*(\\w*))*))");
+        try {
+            fr = new FileReader(FILE_PATH);
+            br = new BufferedReader(fr);
+            String sCurrentLine;
+            boolean mayMatch = true;
+            while ((sCurrentLine = br.readLine()) != null) {
+                Matcher applianceMatcher = pattern.matcher(sCurrentLine);
+                if (applianceMatcher.find()) {
+                    applianceMatcher.usePattern(pattern1);
+                    while (applianceMatcher.find() && mayMatch) {
+                        for (Map.Entry<E, Object> entry : criteria.getCriteria().entrySet()) {
+                            E key = entry.getKey();
+                            Object value = entry.getValue();
+                            if (key.toString().equals(applianceMatcher.group(2))) {
+                                if (value.toString().equals(applianceMatcher.group(3))) {
+                                    System.out.println("VALUE" + value);
+                                } else {
+                                    mayMatch = false;
+                                }
+                            }
+                        }
 
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
+
+// you may add your own code here
 
 
 //you may add your own new classes
--- jwd-task01-template/src/main/java/by/tc/task01/entity/criteria/Criteria.java
+++ jwd-task01-template/src/main/java/by/tc/task01/entity/criteria/Criteria.java
@@ -10,7 +10,11 @@ public class Criteria<E> {
 	public void add(E searchCriteria, Object value) {
 		criteria.put(searchCriteria, value);
 	}
-	
+
+	public Map<E, Object> getCriteria() {
+		return criteria;
+	}
+
 	// you may add your own code here
 
 }
